a7deda31aa73555e129805abedc4bea056a63f2a
exileddb <exileddb@gmail.com>
2020-03-23 14:20:45 +0800

    博客主题

---
+++ 404.php
@@ -0,0 +1,48 @@
+﻿<html><head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=11" id="mixia_vpid">
+
+  <title>404 Not Found</title>
+  <meta name=keywords content="404页面">
+  <meta name=description content="一万年太久，只争朝夕">
+  <link rel="icon" type="image/x-icon" href="<?php echo kratos_option('site_ico'); ?>">  
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/static/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/static/css/main.css">
+<script src="<?php bloginfo('template_url'); ?>/static/js/clipboard.min.js"></script>
+
+
+</head>
+<body>
+
+    <span class="mobile btn-mobile-menu">
+        <i class="fa fa-list btn-mobile-menu__icon"></i>
+        <i class="fa fa-angle-up btn-mobile-close__icon hidden"></i>
+    </span>
+	</script><header class="panel-cover" style="background-image: url(<?php bloginfo('template_url'); ?>/static/images/bg.jpg)">
+	
+  <div class="panel-main">
+
+    <div class="panel-main__inner panel-inverted">
+    <div class="panel-main__content">
+	    
+        <a href="<?php echo home_url();?>" title="返回首页"><img src="<?php bloginfo('template_url'); ?>/static/images/few.jpg" width="150" alt="Noisky logo" class="panel-cover__logo logo"></a>
+        <h1 class="panel-cover__title panel-title"><a href="<?php echo home_url();?>" title="link to homepage for noisky">404</a></h1>
+    
+     
+        <hr class="panel-cover__divider">
+        <p class="panel-cover__description">对不起，您要找的页面被小萌丢失了，(＞人＜；)</p>
+        <hr class="panel-cover__divider panel-cover__divider--secondary">
+        
+        <p class="panel-cover__description">欢迎来到我的个人世界!</p>
+
+
+	<p class="footer"><a href="https://exiled.top">绘梦</a> © 2020  </p> 
+      </div>
+    </div>
+    <div class="panel-cover--overlay cover-slate"></div>
+  </div>
+</header>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/static/js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/static/js/main.js"></script>
+</body></html>
---
+++ README.md
@@ -0,0 +1,25 @@
+# kratos
+
+kratos-dream是由kratos-master二次开发而来的。
+
+首页样式：
+
+![drawdream](https://exiled.top/img/screenshot.png)
+
+本次修改内容由代码高亮的样式
+
+展示图如下：
+
+文章页面的代码样式
+
+![](https://exiled.top/img/str.png)
+
+当发布文章为状态时候首页看到的代码样式：
+
+![](https://exiled.top/img/str1.png)
+
+本次还修改了下404页面效果图如下：
+
+![](https://exiled.top/img/str2.png)
+
+演示网站：https://exiled.top
